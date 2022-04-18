7f261688bb6e5331911ad9354ffe7957df3da22c
susankatz <98364172+susankatz@users.noreply.github.com>
2022-04-17 23:19:51 -0400

    help

--- Final/map.js
+++ Final/map.js
@@ -1,69 +1,69 @@
+var width = 1000;
+var height = 1000;
 
-        var width = 1000;
-        var height = 1000;
-
-        var svg = d3.select("#viz")
+var svg = d3.select("#viz")
                     .attr("width", width)
                     .attr("height", height);
 
-        svg.select("#ocean")
+svg.select("#ocean")
                     .attr("width", width)
                     .attr("height", height);
 
-        var map = svg.select("#map");
+var map = svg.select("#map");
 
-        d3.json("world-alpha3.json")
-            .then(function(world) {
+d3.json("world-alpha3.json")
+    .then(function(world) {
 
-            console.log(world);
-            var geoJSON = topojson.feature(world, world.objects.countries);
+    var geoJSON = topojson.feature(world, world.objects.countries);
 
-            geoJSON.features = geoJSON.features.filter(function(d) {
-                        return d.id !== "ATA";
-                        });
-            console.log(geoJSON);
+    geoJSON.features = geoJSON.features.filter(function(d) {
+                return d.id !== "ATA";
+                });
 
-            var proj = d3.geoMercator()
-                        .fitSize([width, height], geoJSON);
-            
-            var path = d3.geoPath()
-                        .projection(proj);
-            var countries = map.selectAll("path")
-                        .data(geoJSON.features);
+
+    var proj = d3.geoMercator()
+                .fitSize([width, height], geoJSON);
+    
+    var path = d3.geoPath()
+                .projection(proj);
+    var countries = map.selectAll("path")
+                .data(geoJSON.features);
+
+ 
+    d3.json("winedata.json")
+        .then(function(wineData) {
+
+            // DO EVERYTHING IN HERE
+
+            var countryColor = d3.scaleSequential(d3.interpolatePuRd)
+                 .domain([85, 91]);
+    
 
             countries.enter().append("path")
-                    .attr("d", path)
-                    .attr("fill", "white")
-                    .attr("stroke-width", 1)
-                    .attr("stroke", "black");
-        
-            // var points = [
-            //     {"name": "Boston", "coords": [-71.0589, 42.3601]},
-            //     {"name": "London", "coords": [-0.1278, 51.5074]}
-            // ];
-            
-            // var dots = map.selectAll("circle")
-            //             .data(points);
+                 .attr("d", path)
+                 .attr("stroke-width", 1)
+                 .attr("stroke", "black")
+                 .attr("fill", function (item) {
+                     var myCountryData = wineData
+                        .filter(function(d) {
+                            return d.country === item.properties.name;
+                        });
+                        console.log()
+                    if (myCountryData.length) {
+                        var blah = d3.mean(myCountryData, function (d) {
+                            return +d.points;
+                        })
+                        return countryColor(blah)
+                    }
+                    else {
+                        return "LightGray";
+                    }
 
-            // dots.enter().append("circle")
-            //             .attr("r", 10)
-            //             .attr("fill", "red")
-            //             .attr("transform", function (d){
-            //               return  "translate(" + proj(d.coords) + ")";
-         
-            //             });
-
-            // var zoom = d3.zoom()
-            //             .translateExtent([[0, 0], [width, height]])
-            //             .scaleExtent([1, 8])
-            //             .on("zoom", zoomed);
-
-            // function zoomed(event) {
-            //         map.attr("transform", event.transform);
-            // }
-
-            // svg.call(zoom);
-            
-            
-        });
+                
+                });
+        }); });
    
+            
+            
+      
+
